0e209b2f2cafb88cb5539b175998d36919320ec6
avsudnichnikov <avsudnichnikov@gmail.com>
2021-11-01 13:43:35 +0500

    problem

--- app.js
+++ app.js
@@ -13,41 +13,34 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 const port = process.env.PORT || 3000;
 
-// the splitter function, used by the service
 async function getIndex(request, response) {
     response.render('index');
 }
 
-// the service
-const serviceObject = {
+const postPerson = function (args, callback) {
+    callback({
+        greeting: "Hello Cutie Pie!!!" + " " + args.name
+    });
+}
+
+const service = {
     PostPersonService: {
-        PostPersonServiceSoapPort: {
-            MessageSplitter: () => {
-                return {key: '12345'}
-            }
+        PostPersonService_0: {
+            PostPerson: postPerson,
         },
-        PostPersonServiceSoap12Port: {
-            MessageSplitter: {
-                MessageSplitter: () => {
-                    return {key: '12345'}
-                }
-            },
-        }
     }
 };
 
 app.route('/')
     .get(getIndex)
-//    .post(postPerson);
-// http//github.com/officer-rosmarino/node-soap-example/blob/master/service.wsdl
-// http//stackoverflow.com/questions/4791794/client-to-send-soap-request-and-receive-response
-// http//habr.com/ru/post/187390/
-// https://stackoverflow.com/questions/47078343/node-soap-cannot-read-property-description-of-undefined
 
-// Launch the server and listen
-app.listen(port, function () {
+const cbServ = () => {
     console.log('Listening on port ' + port);
-    soap.listen(app, wsdlPath, serviceObject, wsdl, () => {
-        console.log('Check http://localhost:' + port + wsdlPath + '?wsdl to see if the service is working');
-    });
-})
+}
+const cbSoap = () => {
+    console.log('WSDL on http://localhost:' + port + wsdlPath + '?wsdl');
+}
+
+app.listen(port, cbServ);
+
+soap.listen(app, wsdlPath, service, wsdl, cbSoap);
--- static/js/sendForm.js
+++ static/js/sendForm.js
@@ -5,18 +5,10 @@ function bindForm() {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         const xhr = new XMLHttpRequest();
-        const text = `
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-    <soap:Body xmlns="http://localhost/wsdl">
-        <person>
-            <firstName>Nadya</firstName>
-        </person>
-    </soap:Body>
-</soap:Envelope>`;
+        const text = form.querySelector('textarea').value;
         xhr.open('post', url);
         xhr.setRequestHeader('Content-Type', 'text/xml');
         xhr.send(text);
-        console.log(xhr)
         xhr.addEventListener('readystatechange', function () {
             if (this.readyState === this.DONE && this.status === 200) {
                 console.log(this.responseText);
