b5c03be1f1834285e62b9999617e6ab38bca6fb7
avsudnichnikov <avsudnichnikov@gmail.com>
2021-12-20 02:04:48 +0500

    fic code style

--- static/js/sendForm.js
+++ static/js/sendForm.js
@@ -1,25 +1,25 @@
 function bindForm() {
-    const url = '/wsdl';
-    const form = document.querySelector('#soapForm');
-    const responseField = document.querySelector('#responseField');
+  const url = '/wsdl';
+  const form = document.querySelector('#soapForm');
+  const responseField = document.querySelector('#responseField');
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const xhr = new XMLHttpRequest();
-        const text = form.querySelector('textarea').value;
-        xhr.open('post', url);
-        xhr.setRequestHeader('Content-Type', 'text/xml');
-        xhr.send(text);
-        xhr.addEventListener('readystatechange', function () {
-            if (this.readyState === this.DONE) {
-                responseField.innerHTML = this.responseText
-                    .replace(/ ?</g, '&lt;')
-                    .replace(/>&lt;/g, '><br>&lt;');
-            }
-        });
-    })
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const xhr = new XMLHttpRequest();
+    const text = form.querySelector('textarea').value;
+    xhr.open('post', url);
+    xhr.setRequestHeader('Content-Type', 'text/xml');
+    xhr.send(text);
+    xhr.addEventListener('readystatechange', function () {
+      if (this.readyState === this.DONE) {
+        responseField.innerHTML = this.responseText
+          .replace(/ ?</g, '&lt;')
+          .replace(/>&lt;/g, '><br>&lt;');
+      }
+    });
+  })
 }
 
 window.addEventListener('load', () => {
-    bindForm();
+  bindForm();
 })
